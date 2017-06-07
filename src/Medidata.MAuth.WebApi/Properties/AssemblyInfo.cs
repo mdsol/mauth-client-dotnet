a6a1c0a0f851e0a498a2bc874a88d76c86779fcf
Laszlo Schreck <lschreck@mdsol.com>
2017-06-07 16:49:09 +0900

    Change to the new csproj format

--- src/Medidata.MAuth.WebApi/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Medidata.MAuth.WebApi")]
-[assembly: AssemblyDescription("An HTTP message handler for the core Medidata.MAuth module.")]
-[assembly: Guid("487a299d-9de1-4a40-aa43-d4a89e3edc96")]
