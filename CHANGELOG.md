fe32c60b6dffacd1ed537dd26660563c0c39c100
Prajon <pshakya@mdsol.com>
2020-07-10 11:26:51 -0400

    Implemented feedbacks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changes in Medidata.MAuth
 
-## v4.0.3
+## v4.1.0
  - **[Core]** Added normalization of Uri AbsolutePath.
  - **[Core]** Added unescape step in query_string encoding to remove `double encoding`.
 
--- src/Medidata.MAuth.Core/MAuthCoreExtensions.cs
+++ src/Medidata.MAuth.Core/MAuthCoreExtensions.cs
@@ -207,7 +207,12 @@ namespace Medidata.MAuth.Core
                 encodedQueryStrings.Add($"{escapedKey}={escapedValue}");
             });
             var result = string.Join("&", encodedQueryStrings);
-            return result.Replace("%2B","%20");
+
+            // Above encoding converts space as `%20` and `+` as `%2B`
+            // But space and `+` both needs to be converted as `%20` as per
+            // reference https://github.com/mdsol/mauth-client-ruby/blob/master/lib/mauth/request_and_response.rb#L113
+            // so this convert `%2B` into `%20` to match encodedqueryparams to that of other languages.
+            return result.Replace("%2B", "%20");
         }
 
         /// <summary>
@@ -233,8 +238,6 @@ namespace Medidata.MAuth.Core
             // Replacing "//" and "///" into single "/"
             normalizedPath = normalizedPath.Replace("///", "/").Replace("//", "/");
 
-            if (!normalizedPath.EndsWith("/") && (path.EndsWith("/") || path.EndsWith("/.") || path.EndsWith("/..")))
-                normalizedPath = $"{normalizedPath}/";
             return normalizedPath; 
         }
     }
