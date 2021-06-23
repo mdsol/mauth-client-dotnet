488b53bc7ca9600e965da4eb8e8ca924ec297be6
yongliu-mdsol <yoliu@mdsol.com>
2021-06-23 16:28:56 -0400

    create extension for aspnet core objects

---
+++ src/Medidata.MAuth.AspNetCore/UtilityExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Medidata.MAuth.Core;
+
+namespace Medidata.MAuth.AspNetCore
+{
+    /// <summary>
+    /// Extensions for AspNetCore objects
+    /// </summary>
+    public static class UtilityExtensions
+    {
+        /// <summary>
+        /// Determines the correct MAuthHeader value with default check for V2.
+        /// Mauth.Core extension doesn't have extension for HttpRequest
+        /// </summary>
+        /// <param name="request">Aspnet core HttpRequest</param>
+        /// <returns>The MAuthHeader value.</returns>
+        public static string GetAuthHeaderValue(this HttpRequest request)
+        {
+            var requestMessage = request.ToHttpRequestMessage();
+
+            return requestMessage.GetAuthHeaderValue();
+        }
+    }
+}
