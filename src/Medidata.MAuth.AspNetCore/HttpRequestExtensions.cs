3c8054c1ff245c9e54483f93510667300172146e
yongliu-mdsol <yoliu@mdsol.com>
2021-07-07 16:43:34 -0400

    renamed class and add a new extension method

---
+++ src/Medidata.MAuth.AspNetCore/HttpRequestExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Medidata.MAuth.Core;
+
+namespace Medidata.MAuth.AspNetCore
+{
+    /// <summary>
+    /// Extension methods for HttpRequest.
+    /// </summary>
+    public static class HttpRequestExtensions
+    {
+        /// <summary>
+        /// Determines the correct MAuthHeader value with default check for V2.
+        /// </summary>
+        /// <param name="request">HttpRequest</param>
+        /// <returns>Mauth header value from the request.</returns>
+        public static string GetAuthHeaderValue(this HttpRequest request)
+        {
+            if (request.Headers.TryGetValue(Constants.MAuthHeaderKeyV2, out var authHeaderV2))
+            {
+                return authHeaderV2;
+            }
+
+            request.Headers.TryGetValue(Constants.MAuthHeaderKey, out var authHeaderV1);
+            return authHeaderV1;
+        }
+
+        /// <summary>
+        /// Get app uuid from mauth header value in HttpRequest.
+        /// </summary>
+        /// <param name="request">HttpRequest</param>
+        /// <returns>application uuid in mauth header value.</returns>
+        public static Guid GetAuthAppUuid(this HttpRequest request)
+        {
+            var mauthHeader = request.GetAuthHeaderValue();
+            var (appUuid, _) = mauthHeader.ParseAuthenticationHeader();
+            return appUuid;
+        }
+    }
+}
--- tests/Medidata.MAuth.AspNetCoreTests/HttpRequestExtensionsTests.cs
+++ tests/Medidata.MAuth.AspNetCoreTests/HttpRequestExtensionsTests.cs
@@ -1,12 +1,14 @@
-﻿using Medidata.MAuth.AspNetCore;
+﻿using System;
+using Medidata.MAuth.AspNetCore;
 using Medidata.MAuth.Core;
+using Medidata.MAuth.Tests.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Moq;
 using Xunit;
 
 namespace Medidata.MAuth.AspNetCoreTests
 {
-    public class UtilityExtensionsTests
+    public class HttpRequestExtensionsTests
     {
         [Fact]
         public void GetAuthHeaderValue_WithNoMauthHeader_WillReturnNull()
@@ -56,6 +58,42 @@ namespace Medidata.MAuth.AspNetCoreTests
 
             // Assert
             Assert.Equal("v1 header", authHeader);
+        }
+
+        [Fact]
+        public void GetAppUuid_WithV1Header_WillReturnAppUuid()
+        {
+            // Arrange
+            var requestData = "POST".FromResourceSync();
+            var headers = new HeaderDictionary();
+            headers.Add(Constants.MAuthHeaderKey, requestData.MAuthHeader);
+            var mockRequest = new Mock<HttpRequest>();
+            mockRequest.SetupGet(hr => hr.Headers).Returns(headers);
+            var httpRequest = mockRequest.Object;
+
+            // Act
+            var appUuid = httpRequest.GetAuthAppUuid();
+
+            // Assert
+            Assert.Equal(requestData.ApplicationUuid, appUuid);
+        }
+
+        [Fact]
+        public void GetAppUuid_WithV2Header_WillReturnAppUuid()
+        {
+            // Arrange
+            var requestData = "POST".FromResourceV2Sync();
+            var headers = new HeaderDictionary();
+            headers.Add(Constants.MAuthHeaderKey, requestData.MAuthHeader);
+            var mockRequest = new Mock<HttpRequest>();
+            mockRequest.SetupGet(hr => hr.Headers).Returns(headers);
+            var httpRequest = mockRequest.Object;
+
+            // Act
+            var appUuid = httpRequest.GetAuthAppUuid();
+
+            // Assert
+            Assert.Equal(requestData.ApplicationUuid, appUuid);
         }
     }
 }
